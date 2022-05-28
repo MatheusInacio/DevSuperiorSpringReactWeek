d4bdab6e5b5ba0f366b7eb2be795689782b30ecb
Matheus <mmatheus.inacio@outlook.com>
2022-05-28 14:14:50 -0300

    Update requests.ts

--- web/src/utils/requests.ts
+++ web/src/utils/requests.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = import.meta.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080"
+// export const BASE_URL = import.meta.env.REACT_APP_BACKEND_URL ?? "https://dsmovie-matheusinac.herokuapp.com"
+export const BASE_URL = import.meta.env.VITE_APP_BACKEND_URL ?? "http://localhost:8080"
